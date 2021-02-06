a4fd61c92e3d2e57c694bd6a628022983b1ec0b1
Diserton <akanymetov@gmail.com>
2021-02-06 22:50:19 +0600

    initial settings and added scss pack

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <!-- <header></header> -->
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <h1>HELLO</h1>
+          <img alt="Vue logo" src="./assets/logo.png" />
+        </div>
+      </section>
+    </div>
+    <!-- <footer></footer> -->
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script></script>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
